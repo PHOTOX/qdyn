8203dbffc16f2d76f65007e78d7b87ab0c2c2c1f
Jiri Janos <jiri.janos98@gmail.com>
2024-01-16 16:36:50 +0100

    Compilation with FFTW
    1) I added a file with FFTW libraries and file to compile them. It is
       tested on neon and krypton, still not working on Mac.
    
    2) I modified tests suit to export qdyn if not in PATH or at least issue
       an warning.

--- tests/run_test_suite.sh
+++ tests/run_test_suite.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+# tests to be run
 tests="IT_HarmOsc_1D IT_HarmOsc_1D_project_rot IT_Morse_1D IT_HarmOsc_2D_sym IT_HarmOsc_2D_asym IT_HarmOsc_3D_sym IT_HarmOsc_3D_asym RT_HarmOsc_1D"
 
 echo -e "\nRunning test suite"
 
+# checking if qdyn is in PATH and trying to add it if necessary
+if [[ -z $(which qdyn) ]];
+then
+    echo "WARNING: qdyn is not set in your path. Trying to export qdyn to path from ../src."
+    # checking if test are in the same folder as src with qdyn
+    if [[ -z $(ls ../src/qdyn) ]]
+    then
+        echo "ERROR: ../src/qdyn not found. Exiting."
+        exit
+    else
+        pwd=$(pwd | sed 's/tests/src/g')
+        export PATH=$pwd/:$PATH
+        echo "* qdyn exported to path: $(which qdyn)."
+    fi
+fi
+
+# tests
 for i in $tests;
 do
   cd $i
