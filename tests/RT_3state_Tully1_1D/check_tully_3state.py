103817771090dd27ef353c5acbd8700e1e5fbf28
Jiri Janos <jiri.janos98@gmail.com>
2025-01-28 16:07:25 +0100

    Adding 3-state Tully1 test for numerical diagonalization of expH1

---
+++ tests/RT_3state_Tully1_1D/check_tully_3state.py
@@ -0,0 +1,49 @@
+from os.path import exists
+
+from numpy import genfromtxt, linspace, interp, std, abs
+
+### inputs
+pop_thresh = 1e-5  # threshold for the populations std
+ethresh = 5e-5  # threshold for the energy std and drift
+
+### checking populations
+# reading calculated data
+input_file = 'pop_ad.dat'
+if not exists(input_file):
+    print(f'ERROR: {input_file:s} not found!')
+    exit(1)
+pop_ad = genfromtxt(input_file).T
+
+# reading reference from Tomas Jira and his ACRON code
+input_file = 'reference_ad_pop.dat'
+if not exists(input_file):
+    print(f'ERROR: {input_file:s} not found!')
+    exit(1)
+ref_pop = genfromtxt(input_file).T
+
+# interpolating data to have the same grid
+t = linspace(max([pop_ad[0, 0], ref_pop[0, 0]]), min([pop_ad[0, -1], ref_pop[0, -1]]), 1000)
+pop_ad_int = [interp(x=t, xp=pop_ad[0], fp=pop_ad[1 + state]) for state in range(3)]
+pop_ref_int = [interp(x=t, xp=ref_pop[0], fp=ref_pop[1 + state]) for state in range(3)]
+
+# calculating standard deviation
+pop_std = max([std(pop_ad_int[state] - pop_ref_int[state]) for state in range(3)])
+
+### checking energies
+# reading energies
+energy = genfromtxt('energies.dat').T
+
+# calculating energy standard deviation
+energy_std = std(energy[1])
+
+# calculating energy drift in percent of initial total energy
+energy_drift = abs((energy[1, 0] - energy[1, -1]) / energy[1, 0])
+
+### deciding result
+if (pop_std < pop_thresh) and (energy_drift < ethresh) and (energy_std < ethresh):
+    result = True
+else:
+    result = False
+
+# printing result: True/False
+print(result)
